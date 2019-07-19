9d12302c61d4fb780b083b1ccc9b2333a48b1b1b
Phan Thanh Vi <1660710@student.hcmus.edu.vn>
2019-07-19 14:59:04 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,177 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import TaskForm from './components/TaskForm';
+import Control from './components/Control';
+import TaskList from './components/TaskList'
 import './App.css';
+class App extends Component{
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         tasks:[],
+         //dong mo form add edit
+         isDisplayForm:false,
+         taskEditing:null
+      };
+    };
+    //dc goi 1 lan khi f5
+    componentWillMount(){
+        if(localStorage && localStorage.getItem('tasks')){
+            //sang object
+            var tasks = JSON.parse(localStorage.getItem('tasks'));
+            this.setState({
+                tasks:tasks
+            });
+        }
+    }
+    onGenerateData = ()=>{
+        var tasks = [
+            {
+                id: this.generateID(),
+                name: 'lap trinh',
+                status: true
+            },
+            {
+                id:this.generateID(),
+                name:'boi',
+                status: false
+            }
+        ];
+        this.setState({
+            tasks:tasks
+        });
+        localStorage.setItem('tasks',JSON.stringify(tasks));
+    }
+    s4(){
+        return Math.floor((1+Math.random())*0x10000).toString(16).substring(1);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    }
+    generateID(){
+        return this.s4()+this.s4()+this.s4()+this.s4();
+    }
+    onToggleForm=()=>{
+        this.setState({
+            isDisplayForm:!this.state.isDisplayForm
+        });
+    }
+    onCloseForm=()=>{
+        this.setState({
+            isDisplayForm:false
+        });
+    }
+    onSubmit=(data)=>{
+        var {tasks} = this.state;
+        if(data.id===''){
+            data.id=this.generateID();
+            tasks.push(data);
+        }
+        else{
+            var index=this.findIndex(data.id);
+            tasks[index]=data;
+        }
+        
+        this.setState({
+            tasks:tasks,
+            taskEditing:null
+        });
+        localStorage.setItem('tasks',JSON.stringify(tasks));
+        console.log(data.id);
+
+    }
+    onUpdateStatus=(id)=>{
+        // console.log(id);
+        var index=this.findIndex(id);
+        var {tasks}=this.state;
+        if(index!==-1)
+        {
+            tasks[index].status=!tasks[index].status;
+            this.setState({
+                tasks:tasks
+            });
+            localStorage.setItem('tasks',JSON.stringify(tasks));
+        }
+    }
+    findIndex=(id)=>{
+        var {tasks}=this.state;
+        var result=-1;
+        tasks.forEach((task, index)=>{
+            if(task.id===id){
+                return result=index;
+            }
+        });
+        return result;
+    }
+    onDelete=(id)=>{
+        var index=this.findIndex(id);
+        var {tasks}=this.state;
+        if(index!==-1)
+        {
+            tasks.splice(index, 1);
+            // tasks[index].status=!tasks[index].status;
+            this.setState({
+                tasks:tasks
+            });
+            localStorage.setItem('tasks',JSON.stringify(tasks));
+        }
+        // this.onCloseForm();
+    }
+    onShowForm=()=>{
+        this.setState({
+            isDisplayForm:true
+        });
+    }
+    onUpdate=(id)=>{
+        // console.log(id);
+        var index=this.findIndex(id);
+        var {tasks}=this.state;
+        var taskEditing=tasks[index];
+        this.setState({
+            taskEditing:taskEditing
+        });
+        this.onShowForm();
+    }
+  render(){
+      
+       var {tasks, isDisplayForm, taskEditing}=this.state; //=this.state.tasks
+       var elmTaskForm = isDisplayForm?<TaskForm 
+                                        onSubmit={this.onSubmit} 
+                                        task={taskEditing}
+                                        onCloseForm={this.onCloseForm}/>:'';
+
+    return(
+      <div className="container">
+        <div className="text-center">
+            <h1>Quản Lý Công Việc</h1>
+            <hr/>
+        </div>
+        <div className="row">
+            <div className={isDisplayForm?"col-lg-4":""}>
+                {elmTaskForm}
+            </div>
+            <div className={isDisplayForm?"col-lg-8":"col-lg-12"}>
+                <button type="button" className="btn btn-primary" onClick={this.onToggleForm}>
+                    <span className="fa fa-plus mr-5"></span>Thêm Công Việc
+                </button>
+                <button type="button" className="btn btn-danger ml-5" onClick={this.onGenerateData}>
+                    <span>Reset</span>
+                </button>
+                <div className="row mt-15">
+                    <Control />
+                </div>
+                <div className="row mt-15">
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <TaskList tasks={tasks} onUpdateStatus={this.onUpdateStatus}
+                        onDelete={this.onDelete}
+                        onUpdate={this.onUpdate}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  );
+    )
+  }
 }
+
 
 export default App;
---
+++ src/components/TaskForm.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+class TaskForm extends Component{
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         id:'',
+         name:'',
+         status:true
+      };
+    };
+    
+    onCloseForm=()=>{
+        this.props.onCloseForm();
+    }
+    onChange=(event)=>{
+        var target = event.target;
+        var name = target.name;
+        var value = target.value;
+        if(name==='status'){
+            value=target.value==='true'?true:false;
+        }
+        this.setState({
+            [name]:value
+        });
+    }
+    onSubmit=(event)=>{
+        event.preventDefault();
+        this.props.onSubmit(this.state);
+        //huy bo va close form
+        this.onClear();
+        this.onCloseForm();
+    }
+    onClear=()=>{
+        this.setState({
+            name: '',
+            status: false
+        });
+    }
+    componentWillMount(){
+        if(this.props.task){
+            this.setState({
+                id: this.props.task.id,
+                name: this.props.task.name,
+                status: this.props.task.status
+            });
+        }
+    }
+    render(){
+        var {id}=this.state;
+        // console.log(id);
+    return(
+        <div className="panel panel-warning">
+        <div className="panel-heading">
+            <h3 className="panel-title">
+                {/* {id} */}
+                {id===''?'Them cong viec':'Cap nhat cong viec'}
+                <span className="fa fa-times-circle text-right" onClick={this.onCloseForm}></span>
+            </h3>
+        </div>
+        <div className="panel-body">
+            <form onSubmit = {this.onSubmit}>
+                <div className="form-group">
+                    <label>Tên :</label>
+                    <input 
+                    type="text" className="form-control"
+                    name='name' value={this.state.name}
+                    onChange={this.onChange}
+                     />
+                </div>
+                <label>Trạng Thái :</label>
+                <select className="form-control" 
+                name='status' value={this.state.status}
+                onChange={this.onChange}
+                required="required">
+                    <option value={true}>Kích Hoạt</option>
+                    <option value={false}>Ẩn</option>
+                </select>
+                <br/>
+                <div className="text-center">
+                    <button type="submit" className="btn btn-warning">Save</button>&nbsp;
+                    <button type="button" className="btn btn-danger" onClick={this.onClear}>Hủy Bỏ</button>
+                </div>
+            </form>
+        </div>
+        </div>
+    )
+  }
+}
+export default TaskForm;
